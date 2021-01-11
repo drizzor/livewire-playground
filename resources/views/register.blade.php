74c725707138b6184d42c77597f8086ddaf8948e
Kevin <k.mary@live.be>
2021-01-11 10:53:27 +0100

    Laravel & Livewire installation

---
+++ resources/views/register.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Livewire Playground</title>
+    <livewire:styles />
+</head>
+<body>
+   
+    <livewire:scripts />
+</body>
+</html>
