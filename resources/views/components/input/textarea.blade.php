66061747176db638ed9ac97d0ce4853c6e33581c
Kevin <k.mary@live.be>
2021-02-03 11:39:53 +0100

    extracting reusable Blade components for inputs

---
+++ resources/views/components/input/textarea.blade.php
@@ -0,0 +1,6 @@
+<div class="max-w-lg flex rounded-md shadow-sm">
+    <textarea 
+        {{ $attributes }}
+        class="form-textarea block py-3 pl-2 w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+    </textarea>
+</div>
